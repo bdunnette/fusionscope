b30ab6c8a655bc6b689157ef7b830c4525d0d1eb
Brian Dunnette <brian@dunnette.us>
2014-03-20 11:18:24 -0500

    retrieving slide data from fusion table

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,4 +13,15 @@ angular.module('fusionscopeApp', [
       .otherwise({
         redirectTo: '/'
       });
+  })
+  .run(function($rootScope, $http) {
+    var apiKey = 'AIzaSyBS3nzHjd_ghBBVaCRnSy9pulBM0xkTRI4';
+    var tableId = '1DsEEiyukvoOF8zUAKESz5MaOZjIRJBCEgp_d3GmB';
+    $http.get('https://www.googleapis.com/fusiontables/v1/query?sql=SELECT * FROM ' + tableId + '&key=' + apiKey).success(function(data, status, headers, config){
+        console.debug(data);
+        $rootScope.columns = data.columns;
+        $rootScope.rows = data.rows;
+    }).error(function(){
+        console.debug("error");
+    });;
   });
